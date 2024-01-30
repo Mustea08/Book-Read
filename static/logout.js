f4da56968f1d0c014a96d66f9e944a09e358df3d
Mustea08 <mustafa.mohamed2021b@gmail.com>
2024-01-30 13:46:00 +0200

    modified the name of the variable

--- static/logout.js
+++ static/logout.js
@@ -1,18 +1,18 @@
 const logout = document.getElementById("logout");
 // ==for the hamburger button icon
 let hamburgerBtn = document.getElementById("btn");
-const closebutton = document.getElementById("bars");
+const bars = document.getElementById("bars");
 let x = true;
 
 hamburgerBtn.addEventListener("click", () => {
   x = !x;
   if (x) {
     // closebutton.classList.remove("fa-rectangle-xmark");
-    closebutton.classList.remove("fa-regular", "fa-x");
-    closebutton.classList.add("fa-solid", "fa-bars");
+    bars.classList.remove("fa-regular", "fa-x");
+    bars.classList.add("fa-solid", "fa-bars");
   } else {
-    closebutton.classList.remove("fa-solid", "fa-bars");
-    closebutton.classList.add("fa-regular", "fa-x");
+    bars.classList.remove("fa-solid", "fa-bars");
+    bars.classList.add("fa-regular", "fa-x");
   }
 });
 
