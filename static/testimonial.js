4f6d3d002d4cb48020af3cf7e3229cc00ec7c9fb
Mustea08 <mustafa.mohamed2021b@gmail.com>
2024-01-31 18:53:25 +0200

    rename some functions and variables

--- static/testimonial.js
+++ static/testimonial.js
@@ -6,16 +6,16 @@ const invalidName = document.querySelector(".invalidName");
 const invalidFeedback = document.querySelector(".invalidFeedback");
 
 // testimony
-function  setTestimony() {
+function  addTestimony() {
   const nameValue = inputName.value.trim();
   const feedback = inputFeedback.value.trim();
   if (nameValue.length > 2 && feedback.length > 9) {
-    const subParent = document.createElement("div");
-    const child = document.createElement("div");
+    const testimonyParent = document.createElement("div");
+    const testimonyCard = document.createElement("div");
     const names = document.createElement("h5");
     const textFeedback = document.createElement("p");
-    subParent.className = "col-md-4 col-sm-12 my-1";
-    child.className = "card p-2 text-center";
+    testimonyParent.className = "col-md-4 col-sm-12 my-1";
+    testimonyCard.className = "card p-2 text-center";
     let number = localStorage.length + 1;
     // store data to local storage
     localStorage.setItem(
@@ -24,9 +24,9 @@ function  setTestimony() {
     );
     names.innerText = "-" + nameValue;
     textFeedback.innerText = feedback;
-    child.append(textFeedback, names);
-    subParent.appendChild(child);
-    parentContainer.appendChild(subParent);
+    testimonyCard.append(textFeedback, names);
+    testimonyParent.appendChild(testimonyCard);
+    parentContainer.appendChild(testimonyParent);
     inputName.value = "";
     inputFeedback.value = "";
   } else {
@@ -45,28 +45,28 @@ function  setTestimony() {
   }
 }
 
-submitBtn.addEventListener("click", setTestimony);
+submitBtn.addEventListener("click", addTestimony);
 
 //displaying the the feedback in the testimony page
-function callTestimony(){
+function displayTestimony(){
   for (let index = 0; index < localStorage.length; index++) {
-    const subParent = document.createElement("div");
-    const child = document.createElement("div");
+    const testimonyParent = document.createElement("div");
+    const testimonyCard = document.createElement("div");
     const names = document.createElement("h5");
     const textFeedback = document.createElement("p");
-    subParent.className = "col-md-4 col-sm-12 my-1";
-    child.className = "card p-2 text-center";
+    testimonyParent.className = "col-md-4 col-sm-12 my-1";
+    testimonyCard.className = "card p-2 text-center";
     let key = localStorage.key(index);
     if (key != "name") {
       const item = JSON.parse(localStorage.getItem(key));
       textFeedback.innerText = item.feedback;
       names.innerText = "-" + item.name;
-      child.append(textFeedback, names);
-      subParent.appendChild(child);
-      parentContainer.appendChild(subParent);
+      testimonyCard.append(textFeedback, names);
+      testimonyParent.appendChild(testimonyCard);
+      parentContainer.appendChild(testimonyParent);
     }
   }
 }
 
-callTestimony();
+displayTestimony();
 
