5f5e22fd69e778a2165dc30f809eae84fb369d35
Mustea08 <mustafa.mohamed2021b@gmail.com>
2024-01-31 17:46:54 +0200

    rename a function

--- py/helpers.py
+++ py/helpers.py
@@ -13,8 +13,8 @@ def read_json():
     return books
 
 
-# signin file to wirte username in the signin.txt
-def signin(name):
+# signin file to write username in the sign in.txt
+def sign_in(name):
     write_file = open("./files/signin.txt", "+a")
     content = write_file.write(name+ "\n")
     write_file.close()
--- py/routes.py
+++ py/routes.py
@@ -1,6 +1,6 @@
 import datetime
 from flask import render_template, request, send_from_directory
-from py.helpers import read_json, signin
+from py.helpers import read_json, sign_in
 
 current_datetime = datetime.datetime.now()
 formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M")
@@ -11,16 +11,16 @@ def index():
 # ====================================
 
 def home():
-    read_signin = open("./files/signin.txt")
-    data = read_signin.read()
-    read_signin.close()
+    read_sign_in = open("./files/signin.txt")
+    data = read_sign_in.read()
+    read_sign_in.close()
     data_list = data.split("\n")
     data_list.pop()
     books = read_json()
 
     name = request.args.get("name")   #this is for the signin if the is no user yet
     if name:
-        signin(name)
+        sign_in(name)
         return render_template("home.html" , name=name, formatted_datetime=formatted_datetime, books=books)
 
     return render_template("home.html" , data=data_list[len(data_list) -1], formatted_datetime=formatted_datetime, books=books)
